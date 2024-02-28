44db5a5ea429eb8e0bf79eb42b21747646497f74
Peter Hoppe <peterhoppe341@gmail.com>
2024-02-28 14:19:45 -0600

    Polished Spelling of Bio

--- src/components/Bio/Bio.jsx
+++ src/components/Bio/Bio.jsx
@@ -9,14 +9,12 @@ function Bio(props) {
             <div className='right-bio'>  
                 <p><b>Hello and welcome to my portfolio!</b></p>
                 <p>I am a game programmer with cross-disciplinary experience in software development, game design, and user experience design. I learned game design while in college before being hired in web development, where I gained front-end programming and UX skills. These skills leveraged an opportunity to work in VR, allowing me to combine my experience with games in a more business setting.</p>   
-                <p>After five years of professional programming experience, I am returning to my main passion of video games. I recently released a VR game for the Oculus Quest.</p>
-                <p>Below are my projects organized into different focuses. Feel free to click on one and explore the projects I have been a part of to learn more about the work I have done.</p>
+                <p>After five years of professional programming experience, I returned to my first passion of video games. I recently released a VR game for the Oculus Quest.</p>
+                <p>Explore the categories below to learn more about me and my work.</p>
             </div>        
       </div>
     )
 }
-
-//<h1>Peter Hoppe</h1>
 
 export default Bio;
 
