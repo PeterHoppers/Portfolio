7367d10362f50d86ad913658394896e77fdcc5ff
Peter Hoppe <peterhoppe341@gmail.com>
2024-07-15 15:32:25 -0500

    Round 9 of Mobile UI Polish
    - adjusted a bunch of font sizes on the bio side of things

--- src/components/Bio/Bio.jsx
+++ src/components/Bio/Bio.jsx
@@ -7,9 +7,8 @@ function Bio(props) {
                 <h1>Peter Hoppe</h1>
             </div>
             <div className='right-bio'>  
-                <p><b>Hello and welcome to my portfolio!</b></p>
                 <p>I am a programmer with cross-disciplinary experience in software development, game design, and user experience design. I learned game design while in college before being hired in web development, where I gained front-end programming and UX skills. These skills leveraged an opportunity to work in VR, allowing me to combine my experience with games in a more business setting.</p>   
-                <p>After five years of professional programming experience, I returned to my first passion of video games to use those skills learned. This concluded in releasing a VR game for the Oculus Quest.</p>
+                <p>After five years of professional programming experience, I returned to my first passion of video games to use those skills learned. In this time, I have independently programming both VR games and web games.</p>
                 <p>Explore the categories below to learn more about me and my work.</p>
             </div>        
       </div>
