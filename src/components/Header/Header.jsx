ce7f6337a97652dbe3bba8f5f9d46b19aa4e02c1
Peter Hoppe <peterhoppe341@gmail.com>
2024-07-15 11:10:51 -0500

    Added Itch.Io Link
    - added a link to my itch.io

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,6 +2,7 @@
 import linkedInLogo from '../../assets/socials/linkedIn.svg'
 import githubLogo from '../../assets/socials/github.svg';
 import emailLogo from '../../assets/socials/email.svg';
+import itchLogo from '../../assets/socials/itchio.svg';
 import './Header.css';
 
 function Header(props) {  
@@ -11,12 +12,15 @@ function Header(props) {
                 <a href = "mailto:peterhoppe341@gmail.com" target="_blank">
                     <img src={emailLogo} alt="Email Me"/>
                 </a> 
+                <a href = "https://peterhoppe.itch.io/" target="_blank">
+                    <img src={itchLogo} alt="Itch.io Profile"/>
+                </a>    
                 <a href = "https://www.linkedin.com/in/peter-hoppe/" target="_blank">
-                    <img src={linkedInLogo} alt="LinkedIn"/>
+                    <img src={linkedInLogo} alt="LinkedIn Profile"/>
                 </a>   
                 <a href = "https://github.com/PeterHoppers" target="_blank">
-                    <img src={githubLogo} alt="Github"/>
-                </a>                 
+                    <img src={githubLogo} alt="Github Profile"/>
+                </a>
             </div>                 
       </header>
     )
